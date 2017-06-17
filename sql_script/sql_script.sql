b72453a7e9793cc74759b70fca65f07a7f984256
78526Nasir <islam35-1102@diu.edu.bd>
2017-06-17 16:41:35 +0600

    sql script modified

---
+++ sql_script/sql_script.sql
@@ -0,0 +1,12 @@
+create database E_Commerce
+use E_Commerce
+
+create table Account(
+	username	varchar(50) not null,
+	password	varchar(50) not null,
+	
+	constraint pk_un primary key (username)
+)
+
+insert into Account values('Nasir','78526');
+

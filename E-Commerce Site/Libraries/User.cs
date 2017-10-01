f0c5ffaffc944824bec76ad73b58e1c4b369153b
78526Nasir <islam35-1102@diu.edu.bd>
2017-10-01 22:46:40 +0600

    User.cs class added

---
+++ E-Commerce Site/Libraries/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_Site.Libraries
+{
+    public class User
+    {
+        private string _username;
+        private string _password;
+        private string _gender;
+        private string _religion;
+        private string _email;
+        private int _age;
+
+        public User(string username,string password, string gender, string religion, string email, int age)
+        {
+            _username = username;
+            _password = password;
+            _gender = gender;
+            _religion = religion;
+            _email = email;
+            _age = age;
+        }
+
+    }
+}

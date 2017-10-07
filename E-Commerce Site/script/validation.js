8c78e93cdd6fcf5b28a7e9fd688d19fce98cf59a
78526Nasir <islam35-1102@diu.edu.bd>
2017-10-07 09:56:52 +0600

    validation.js file created

--- E-Commerce Site/script/validation.js
+++ E-Commerce Site/script/validation.js
@@ -1,10 +1,15 @@
 ﻿/* Generic function for required field validation*/
 
-function usernameValidation(){
-    if (!document.getElementById("txtUsername").value) {
-        document.getElementById("unAlert").style.visibility = "visible";
+function usernameValidation() {
+    var element = document.getElementById("txtUsername");
+    var alertEle = document.getElementById("unAlert");
+    if (!element.value) {
+        alertEle.style.visibility = "visible";
+        element.style.border = "1px solid red";
+        element.style.boxShadow = "0px 0px 5px rgba(255,0,0,0.4)";
 
     } else {
-        document.getElementById("unAlert").style.visibility = "hidden";
+       alertEle.style.visibility = "hidden";
     }
 }
+

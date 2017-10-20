6c7b722f40c2ccb668ebb26889335586a01079d5
78526Nasir <islam35-1102@diu.edu.bd>
2017-10-20 23:25:35 +0600

    loginValidation.js file created

---
+++ E-Commerce Site/script/loginValidation.js
@@ -0,0 +1,42 @@
+﻿
+/* Adding unobtrusive javascript on button control*/
+/* Adding an extra event listener for register button */
+
+document.addEventListener('DOMContentLoaded', WireUpEvents);
+
+function WireUpEvents() {
+    var button1 = document.getElementById('btnLogin');
+    button1.addEventListener('click', button1_Click);
+}
+
+function usernameValidation() {
+    var element = document.getElementById("txtUsername");
+
+    if (!element.value) {
+        element.style.border = "1px solid red";
+        element.style.boxShadow = "0px 0px 5px rgba(255,0,0,0.4)";
+    } else {
+        element.style.border = "1px solid #ccc;";
+        element.style.boxShadow = "none";
+    }
+}
+
+function passwordValidation() {
+    var element = document.getElementById("txtPassword");
+
+    if (!element.value) {
+        element.style.border = "1px solid red";
+        element.style.boxShadow = "0px 0px 5px rgba(255,0,0,0.4)";
+
+    } else {
+        element.style.border = "1px solid #ccc";
+        element.style.boxShadow = "none";
+    }
+}
+
+
+
+function button1_Click() {
+    usernameValidation();
+    passwordValidation();
+}

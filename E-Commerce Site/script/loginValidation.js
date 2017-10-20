8dd90ca404e4cffc9ee64fc599b389cb0bd89ad3
78526Nasir <islam35-1102@diu.edu.bd>
2017-10-21 00:54:59 +0600

    loginvalidation.js done

--- E-Commerce Site/script/loginValidation.js
+++ E-Commerce Site/script/loginValidation.js
@@ -11,26 +11,32 @@ function WireUpEvents() {
 
 function usernameValidation() {
     var element = document.getElementById("txtUsername");
+    var alertEle = document.getElementById("unAlert");
 
     if (!element.value) {
+        alertEle.style.visibility = "visible";
         element.style.border = "1px solid red";
         element.style.boxShadow = "0px 0px 5px rgba(255,0,0,0.4)";
     } else {
-        element.style.border = "1px solid #ccc;";
+        element.style.border = "1px solid #ccc";
         element.style.boxShadow = "none";
+        alertEle.style.visibility = "hidden";
     }
 }
 
 function passwordValidation() {
     var element = document.getElementById("txtPassword");
+    var alertEle = document.getElementById("pAlert");
 
     if (!element.value) {
+        alertEle.style.visibility = "visible";
         element.style.border = "1px solid red";
         element.style.boxShadow = "0px 0px 5px rgba(255,0,0,0.4)";
 
     } else {
         element.style.border = "1px solid #ccc";
         element.style.boxShadow = "none";
+        alertEle.style.visibility = "hidden";
     }
 }
 

ea7083e4a478449ab9edb99ef831a523dee8eba1
78526Nasir <islam35-1102@diu.edu.bd>
2017-10-20 21:04:19 +0600

    SQL script modified

--- sql_script/sql_script.sql
+++ sql_script/sql_script.sql
@@ -1,6 +1,9 @@
 create database E_Commerce
+
 use E_Commerce
 
+
+-- Acount TABLE --
 create table Account(
 	username	varchar(50) not null,
 	password	varchar(50) not null,
@@ -8,12 +11,14 @@ create table Account(
 	constraint pk_un primary key (username)
 )
 
+
+
 insert into Account values('Nasir','78526');
 insert into Account values('Sujan','2544');
 
+
+-- User TABLE --
 create table [User](
-	--string username, string fullName, string email, string dob, string religion, 
-	--string password, string gender
 	 [user_id]	int identity,	
      username	varchar(50) not null,
      fullname	varchar(100) not null,
@@ -33,3 +38,41 @@ INSERT INTO [USER] VALUES('sujan', 'nasir islam sujan', 's@s.com', 1995-12-17, 2
 
 
 select * from [User]
+
+
+-- Product TABLE --
+
+create table Product
+(
+	Product_ID			int primary key identity,
+	Name				varchar(50),
+	[Description]		varchar(500),
+	Price				varchar(20),
+	ImageUrl			varchar(500),
+	Category_ID			int
+)
+
+drop table Product
+
+
+-- Category TABLE --
+
+create table Category
+(
+	Category_ID				int primary key identity,
+	Category_Name			varchar(50),
+	Category_Description	varchar(500)
+)
+
+drop table Category
+select * from Category
+
+-- Store Procedure for insert data into Category TABLE --
+create proc sp_addNewCategory
+@categoryName varchar(50),
+@categoryDesc varchar(500)
+
+as
+begin
+	insert into Category values(@categoryName,@categoryDesc)
+end

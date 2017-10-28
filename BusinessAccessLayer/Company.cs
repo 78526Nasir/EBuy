09af70b1b3d3cb6e2045b003d2646abfcd5d75c3
78526Nasir <islam35-1102@diu.edu.bd>
2017-10-28 15:39:38 +0600

    Company.cs and Product.cs file added in BAL

---
+++ BusinessAccessLayer/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public string CompanyDescription { get; set; }
+        public string PartnershipDate { get; set; }
+
+        // Properties are initialize dynamically at runtime
+    }
+}
---
+++ BusinessAccessLayer/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductPrice { get; set; }
+        public string ProductImage { get; set; }
+        public string ProductCompany { get; set; }
+
+        // objects are initialize dynamically at runtime
+
+        /*
+            public Product(string productName, string productCategory, string productDescription, string productImage, string productCompany)
+            {
+                ProductName = productName;
+                ProductCategory = productCategory;
+                ProductDescription = productDescription;
+                ProductImage = productImage;
+                ProductCompany = productCompany;
+            }
+
+        */
+
+    }
+}

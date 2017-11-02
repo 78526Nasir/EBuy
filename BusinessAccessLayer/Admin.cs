c26631b2c4ab9f58822e219b3f4da67cfb0da6db
78526Nasir <islam35-1102@diu.edu.bd>
2017-11-02 14:34:53 +0600

    Admin.cs file moved to BALayer

---
+++ BusinessAccessLayer/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessAccessLayer
+{
+    public class Admin
+    {
+        public string UserName { get; set; }
+        public string password;
+        public string FullName { get; set; }
+        public string Image { get; set; }
+
+        // do not use any constructor. object are intialize dynamically on the fly
+
+        public string getPassword()
+        {
+            return password;
+        }
+    }
+}

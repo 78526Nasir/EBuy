fb31e567f9117fe38530782f3d87288d0dccdbb2
78526Nasir <islam35-1102@diu.edu.bd>
2017-11-11 14:55:56 +0600

    editButton.js file created

---
+++ E-Commerce Site/script/editButton.js
@@ -0,0 +1,29 @@
+﻿function btnUEditClick() {
+    var txtEle = document.getElementById("txtUsername");
+    txtEle.disabled = false;
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}
+
+function btnFNEditClick() {
+    var txtEle = document.getElementById("txtFullName");
+    txtEle.disabled = false;
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}
+
+function btnPEditClick() {
+    var txtEle = document.getElementById("txtPassword");
+    txtEle.disabled = false;
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}

b94feaa0dfa0f21706f59f2b872b9f2470f2facf
78526Nasir <islam35-1102@diu.edu.bd>
2017-11-11 14:55:30 +0600

    admin profile page modified

--- E-Commerce Site/admin/Profile.aspx
+++ E-Commerce Site/admin/Profile.aspx
@@ -6,32 +6,79 @@
 <head runat="server">
     <title>Admin Profile</title>
     <meta name="author" content="Nasir Islam Sujan" />
-    <link rel="stylesheet" href="../css/adminProfile.css" type="text/css"/>
+    <link rel="stylesheet" href="../css/adminProfile.css" type="text/css" />
+    <script src="../script/editButton.js" type="text/javascript"></script>
 
 </head>
 <body>
     <form id="form1" runat="server">
 
-    <div id="header">
-        <h1 id="title">Admin Profile</h1>    
-        <asp:Button ID="btnAdminPanel" runat="server" Text="Back to panel" />
-
-    </div>
-    <div id="container">
-        <div id="left-container">
-            <div id="img-container">
-
-            </div>
-            <div id="name-container">
-
+        <div id="header">
+            <h1 id="title">Admin Profile</h1>
+            <div class="btn">
+                <asp:Button ID="btnAdminPanel" runat="server" Text="Back to panel" />
             </div>
         </div>
-            
-        <div id="right-container">
+        <div id="container">
+            <div id="left-container">
+                <div id="img-container">
+                    <asp:Image ID="imgAdmin" ImageUrl="~/images/user.png" runat="server" CssClass="admin-image" AlternateText="Admin Image" />
+                </div>
+                <div id="name-container">
+                    <asp:Label ID="lblAdminName" runat="server">Nasir Islam Sujan</asp:Label>
+                </div>
+            </div>
+
+            <div id="right-container">
+                <div id="right-top">
+                    <h1>Profile Details</h1>
+                </div>
+                <div id="main-content">
+                    <table id="tblAdminProfile">
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblUsername" runat="server" CssClass="label">Something</asp:Label>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="txtUsername" runat="server" CssClass="text-field" disabled="true" ></asp:TextBox>
+                            </td>
+                            <td>
+                                <input type="button" runat="server" value="Edit" id="btnUEdit" class="btn-edit" onclick="btnUEditClick()"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblFullName" runat="server" CssClass="label">Something</asp:Label>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="txtFullName" runat="server" CssClass="text-field" disabled="true"></asp:TextBox>
+                            </td>
+                            <td>
+                                <input type="button" runat="server" value="Edit" id="btnFNEdit" class="btn-edit" onclick="btnFNEditClick()"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblPassword" runat="server" CssClass="label">Something</asp:Label>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="txtPassword" runat="server" CssClass="text-field" disabled="true" ></asp:TextBox>
+                            </td>
+                            <td>
+                                <input type="button" runat="server" value="Edit" id="btnPEdit" class="btn-edit" onclick="btnPEditClick()"/>
+                            </td>
+                        </tr>
+                    </table>
+                    <asp:Button ID="btnUpdate" Text="Update" disabled="true" runat="server"/>
+                </div>
+            </div>
 
         </div>
-
-    </div>
     </form>
+
+    <div class="copyright">
+        <code>This project is designed and developed by <strong><a href="https://github.com/78526Nasir">Nasir islam Sujan</a> &copy;</strong>2017</code>
+    </div>
+
 </body>
 </html>
--- E-Commerce Site/admin/Profile.aspx.designer.cs
+++ E-Commerce Site/admin/Profile.aspx.designer.cs
@@ -29,5 +29,113 @@ namespace E_Commerce_Site.admin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnAdminPanel;
+        
+        /// <summary>
+        /// imgAdmin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgAdmin;
+        
+        /// <summary>
+        /// lblAdminName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAdminName;
+        
+        /// <summary>
+        /// lblUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+        
+        /// <summary>
+        /// txtUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsername;
+        
+        /// <summary>
+        /// btnUEdit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputButton btnUEdit;
+        
+        /// <summary>
+        /// lblFullName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFullName;
+        
+        /// <summary>
+        /// txtFullName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFullName;
+        
+        /// <summary>
+        /// btnFNEdit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputButton btnFNEdit;
+        
+        /// <summary>
+        /// lblPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+        
+        /// <summary>
+        /// txtPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// btnPEdit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputButton btnPEdit;
+        
+        /// <summary>
+        /// btnUpdate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnUpdate;
     }
 }

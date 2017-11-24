a60c09c000055416efebfe183b6572bda37a4ab3
78526Nasir <islam35-1102@diu.edu.bd>
2017-11-25 02:04:50 +0600

    ForegetPasswordRedirect.aspx page created

---
+++ E-Commerce Site/UI/ForgetPasswordRedirect.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ForgetPasswordRedirect.aspx.cs" Inherits="E_Commerce_Site.UI.ForgetPasswordRedirect" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Reset password</title>
+    <link rel="stylesheet" href="../css/f_p_r.css" type="text/css"/>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h1>Reset Password</h1>
+            <asp:TextBox ID="txtEmail" runat="server" placeholder="Enter your email" CssClass="text-box" TextMode="Email"></asp:TextBox>
+            <br/>
+            <asp:RequiredFieldValidator CssClass="validation" ID="rfv1" runat="server" ControlToValidate="txtEmail" ErrorMessage="Email required"></asp:RequiredFieldValidator>
+            <br />
+            <asp:Button ID="btnResetPassword" runat="server" Text="Reset Password" CssClass="button" OnClick="btnResetPassword_Click"/>
+            <br/>
+            <asp:Label ID="lblStatus" runat="server" CssClass="label" ></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ E-Commerce Site/UI/ForgetPasswordRedirect.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessAccessLayer;
+using System.Data;
+
+namespace E_Commerce_Site.UI
+{
+    public partial class ForgetPasswordRedirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnResetPassword_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbEmail = new StringBuilder();
+            sbEmail.Append(txtEmail.Text);
+
+            User user = new User(sbEmail);
+            ECommerceBusiness ecb = new ECommerceBusiness
+            {
+                UserObj = user
+            };
+
+            DataTable dt = ecb.resetPassword();
+
+            if (dt.Rows.Count > 0)
+            {
+                string returnCode = dt.Rows[0]["ReturnCode"].ToString();
+                if (returnCode.Equals("1"))
+                {
+                    string username = dt.Rows[0]["Username"].ToString();
+                    string toEmail = dt.Rows[0]["Email"].ToString();
+                    string uniqueID = dt.Rows[0]["UniqueID"].ToString();
+
+                    try
+                    {
+                        SendPasswordResetMail(toEmail, username, uniqueID);
+                        lblStatus.Text = "An email with instructions to reset your password is sent to your email address";
+                    }
+                    catch(Exception ex)
+                    {
+                        lblStatus.Text = "An unknown error occured!";
+                    }
+                    
+                }else
+                {
+                    lblStatus.Text = "Email not found!";
+                    lblStatus.ForeColor = System.Drawing.Color.Red;
+                }
+
+            }else
+            {
+                lblStatus.Text = "Email not found!";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+           
+        }
+
+        private void SendPasswordResetMail(string toEmail, string username,string uniqueID)
+        {
+            MailMessage mailMessage = new MailMessage("nasirislam1996@gmail.com", toEmail);
+            StringBuilder sbEmailBody = new StringBuilder();
+
+            sbEmailBody.Append("Dear " + username + ",<br/><br/>");
+            sbEmailBody.Append("Please click on the following link to reset your password");
+            sbEmailBody.Append("<br/>");
+            sbEmailBody.Append("http://localhost:18523/UI/ResetPassword.aspx?uid="+uniqueID);
+            sbEmailBody.Append("<br/><br/>");
+            sbEmailBody.Append("<b>E-Buy</b>");
+
+            mailMessage.IsBodyHtml = true; 
+            mailMessage.Body = sbEmailBody.ToString();
+            mailMessage.Subject = "Reset your password";
+
+            /*
+             * SMTP (Simple Mail Transfer Protocol) is responsible for
+             * sending and receiving email
+             * here we use Gmail SMTP Client for sending email
+             * Gmail SMTP Client host => smtp.gmail.com
+             * port => 587
+             */
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.Credentials = new NetworkCredential()
+            {
+                UserName = "nasirislam1996@gmail.com",
+                Password = "sujan@gmail4"
+            };
+
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+        }
+    }
+}

0dc85164ccef55c1d37e6d31328aab6f990cf64b
78526Nasir <islam35-1102@diu.edu.bd>
2017-12-09 16:32:35 +0600

    javascript for edit button modified

--- E-Commerce Site/script/editButton.js
+++ E-Commerce Site/script/editButton.js
@@ -20,6 +20,50 @@ function btnFNEditClick() {
 
 function btnPEditClick() {
     var txtEle = document.getElementById("txtPassword");
+    txtEle.setAttribute('type', 'password');
+    txtEle.disabled = false;
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}
+
+function btnEEditClick() {
+    var txtEle = document.getElementById("txtEmail");
+    txtEle.disabled = false;
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}
+
+function btnDOBEditClick() {
+    var txtEle = document.getElementById("txtDOB");
+    txtEle.disabled = false;
+    txtEle.setAttribute('type', 'date');
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}
+
+
+function btnREditClick() {
+    var txtEle = document.getElementById("txtReligion");
+    txtEle.disabled = false;
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}
+
+
+function btnGEditClick() {
+    var txtEle = document.getElementById("ddlGender");
     txtEle.disabled = false;
     txtEle.style.cursor = "default";
     txtEle.style.background = "white";

3c7462d2f0cc61bce96cd908cf7b02c405d62d06
78526Nasir <islam35-1102@diu.edu.bd>
2017-12-10 16:56:06 +0600

    Cart class added

---
+++ BusinessAccessLayer/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer
+{
+    public class Cart
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+    }
+}

f4c792ec1b2d6ce01fe776f1b5edb2fbb215f137
78526Nasir <islam35-1102@diu.edu.bd>
2017-12-21 18:45:57 +0600

    Adding one Web Service to check whether the username available or not

---
+++ E-Commerce Site/WebServices/IsUserNameExists.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="IsUserNameExists.asmx.cs" Class="E_Commerce_Site.WebServices.IsUserNameExists" %>
---
+++ E-Commerce Site/WebServices/IsUserNameExists.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using BusinessAccessLayer;
+using System.Data;
+using System.Web.Script.Serialization;
+
+namespace E_Commerce_Site.WebServices
+{
+    /// <summary>
+    /// IsUserNameExists going to provide us the information
+    /// whether an username was already exists or not
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class IsUserNameExists : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public void UserNameExists(string username)
+        {
+            User user = new User(username);
+            user.UserNameInUse = false;
+
+            ECommerceBusiness ecb = new ECommerceBusiness
+            {
+                UserObj = user
+            };
+
+            DataTable dt = ecb.IsUserNameExists();
+
+            if (dt.Rows.Count > 0)
+            {
+                user.UserNameInUse = dt.Rows[0]["UserNameExists"].ToString().Equals("0") ? false : true;
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(user));
+        }
+    }
+}

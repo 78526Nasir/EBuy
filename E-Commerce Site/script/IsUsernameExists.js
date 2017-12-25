d4d37807b85a60c46ecac6db1fa835c4845109b6
78526Nasir <islam35-1102@diu.edu.bd>
2017-12-26 01:58:09 +0600

    wirtting ajax code to check whether a given username is in the database or not.

---
+++ E-Commerce Site/script/IsUsernameExists.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+    $("#txtUsername").keyup(function () {
+        var username = $(this).val();
+        var label = $("#lblAvailablityMessage");
+
+        if (username.length >= 3) {
+            $.ajax({
+                url: "../WebServices/IsUserNameExists.asmx/UserNameExists",
+                method: "post",
+                data: { username: username },
+                dataType: "json",
+                success: function (data) {
+                    if (data.UserNameInUse) {
+                        label.text(username + " is already in use");
+                        label.css("color", "red");
+                    } else {
+                        label.text(username + " is available");
+                        label.css("color", "green");
+                    }
+                },
+                error: function (err) {
+                    alert(err);
+                }
+            });
+
+        }
+
+        if (username.length <= 2) {
+            label.text("");
+        }
+    });
+});

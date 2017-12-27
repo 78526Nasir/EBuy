5d0a703028d28c72ffcb6b6c37bf348677db1587
78526Nasir <islam35-1102@diu.edu.bd>
2017-12-28 03:13:52 +0600

    WebService for checking available emails

---
+++ E-Commerce Site/WebServices/IsEmailExists.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="IsEmailExists.asmx.cs" Class="E_Commerce_Site.WebServices.IsEmailExists" %>
---
+++ E-Commerce Site/WebServices/IsEmailExists.asmx.cs
@@ -0,0 +1,49 @@
+﻿using BusinessAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+
+namespace E_Commerce_Site.WebServices
+{
+    /// <summary>
+    /// IsEmailExists WebService going to provide us the information 
+    /// whether a given Email already exists in the database or not.
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class IsEmailExists : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void EmailExists(string email)
+        {
+            User user = new User
+            {
+                Email = email
+            };
+
+            user.EmailInUse = false;
+
+            ECommerceBusiness ecb = new ECommerceBusiness
+            {
+                UserObj = user
+            };
+
+            DataTable dt = ecb.IsEmailExists();
+
+            if (dt.Rows.Count > 0)
+            {
+                user.EmailInUse = dt.Rows[0]["EmailExists"].ToString().Equals("0") ? false : true;
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(user));
+        }
+    }
+}

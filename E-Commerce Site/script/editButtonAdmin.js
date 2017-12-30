58f0aedbedd7dc81f9039ecfc971e8853c0e7c61
78526Nasir <islam35-1102@diu.edu.bd>
2017-12-31 03:34:26 +0600

    javascript for admin profile edit button

---
+++ E-Commerce Site/script/editButtonAdmin.js
@@ -0,0 +1,33 @@
+﻿
+function btnUEditClick() {
+    var txtAEle = document.getElementById("txtUsername");
+    txtAEle.disabled = false;
+    txtAEle.style.cursor = "default";
+    txtAEle.style.background = "white";
+
+    var btnAEle = document.getElementById("btnUpdate");
+    btnAEle.disabled = false;
+    btnAEle.style.cursor = "pointer";
+}
+
+function btnFNEditClick() {
+    var txtAEle = document.getElementById("txtFullName");
+    txtAEle.disabled = false;
+    txtAEle.style.cursor = "default";
+    txtAEle.style.background = "white";
+
+    var btnAEle = document.getElementById("btnUpdate");
+    btnAEle.disabled = false;
+    btnAEle.style.cursor = "pointer";
+}
+
+function btnPEditClick() {
+    var txtEle = document.getElementById("txtPassword");
+    txtEle.setAttribute('type', 'password');
+    txtEle.disabled = false;
+    txtEle.style.cursor = "default";
+    txtEle.style.background = "white";
+    var btnEle = document.getElementById("btnUpdate");
+    btnEle.disabled = false;
+    btnEle.style.cursor = "pointer";
+}

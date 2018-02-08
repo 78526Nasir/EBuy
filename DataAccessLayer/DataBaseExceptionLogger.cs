291a3e3d2ec369fbd3a805d7a2946ebb233ca84f
78526Nasir <islam35-1102@diu.edu.bd>
2018-02-09 00:54:16 +0600

    Adding DataBase Exception Logger class

---
+++ DataAccessLayer/DataBaseExceptionLogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DataBaseExceptionLogger
+    {
+
+        /// <summary>
+        /// Logging the Exception into a StringBuilder object.
+        /// And then store it to the Widdows Event Viewr
+        /// </summary>
+        /// <param name="exception">the actual exception</param>
+        public static void Log(Exception exception)
+        {
+            StringBuilder sbExceptionMessage = new StringBuilder();
+
+            do
+            {
+                sbExceptionMessage.Append("********************" + " Exception Log - " + DateTime.Now + " *********************");
+                sbExceptionMessage.Append(Environment.NewLine+Environment.NewLine);
+
+                sbExceptionMessage.Append("Exception Type" + Environment.NewLine);
+                sbExceptionMessage.Append(exception.GetType().Name);
+
+                sbExceptionMessage.Append("Message" + Environment.NewLine);
+                sbExceptionMessage.Append(exception.Message + Environment.NewLine + Environment.NewLine);
+
+                sbExceptionMessage.Append("Stack Trace" + Environment.NewLine);
+                sbExceptionMessage.Append(exception.StackTrace + Environment.NewLine + Environment.NewLine);
+
+                // if the exception has inner exception
+                exception = exception.InnerException;
+
+            } while (exception != null);
+
+            if (EventLog.SourceExists("DotNET"))
+            {
+                EventLog eventLog = new EventLog("EBuy");
+                eventLog.Source = "DotNET";
+                eventLog.WriteEntry(sbExceptionMessage.ToString(), EventLogEntryType.Error);
+            }
+        }
+    }
+}

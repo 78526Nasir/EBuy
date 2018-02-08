d5db36c32d3ff4485670192e2a8c21490050bfbb
78526Nasir <islam35-1102@diu.edu.bd>
2018-02-09 00:56:58 +0600

    Adding Global.asax file

---
+++ E-Commerce Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_Commerce_Site.Global" Language="C#" %>
---
+++ E-Commerce Site/Global.asax.cs
@@ -0,0 +1,67 @@
+﻿using BusinessAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace E_Commerce_Site
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            ExceptionLogger.Log(ex);
+
+            var exception = (HttpException)ex;
+            int httpCode = exception.GetHttpCode();
+
+            Server.ClearError();           
+
+            if (!Response.IsRequestBeingRedirected)
+            {
+                if (httpCode == 404)
+                {
+                    Response.Redirect("~/Errors_UI/PageNotFoundError.aspx");
+                }
+                else
+                {
+                    Response.Redirect("~/Errors_UI/Error.aspx");
+                }
+            }
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
